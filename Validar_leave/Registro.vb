ef66132b3be51d50266215a2a8a48a5771170b8c
Antonio Rodríguez <antonio@iessanvicente.com>
2015-11-10 20:59:35 +0100

    Versión 1.0

--- Validar_leave/Registro.vb
+++ Validar_leave/Registro.vb
@@ -1,6 +1,12 @@
-﻿Public Class frmRegistro
+﻿Option Explicit On
+Option Strict On
+''' <summary>
+''' Ejemplo de validación a la hora de abandonar un control
+''' </summary>
 
+Public Class frmRegistro
 
+    'Atributos que nos permiten conocer el estado de validación de los controles.
     Private _ControlNombre As Boolean
     Private _ConrtrolApellidos As Boolean
 
@@ -24,68 +30,24 @@
         pControl.BackColor = Color.LightCoral
         TTip.SetToolTip(pControl, CadenaErrores)
 
-        'Es redundante usar un tooltip y el color con el error provider, pero se pone a modo de ejemplo
+        'Es redundante usar un tooltip y el color con el ErrorProvider, pero se pone a modo de ejemplo
         ErrorProvider1.SetError(pControl, CadenaErrores)
     End Sub
 
-    Public Sub IndicarOk(ByVal pControl As Control, ByVal CadenaErrores As String)
+    Public Sub IndicarOk(ByVal pControl As Control)
+        Dim TTip As ToolTip = New ToolTip()
         pControl.BackColor = Color.White
-
+        TTip.SetToolTip(pControl, "")
+        TTip.Active = False
 
         'Es redundante usar un tooltip y el color con el error provider, pero se pone a modo de ejemplo
         ErrorProvider1.SetError(pControl, "")
     End Sub
 
-    '''' <summary>
-    '''' Validar un formulario se pude hacer de muchas formas, una de ellas es a la hora de pulsar el botón aceptar.
-    '''' Tenemos que validar todos los errores por separado para poder informar al usuario de TODOS los errores cometidos.
-    '''' </summary>
-    '''' <param name="sender"></param>
-    '''' <param name="e"></param>
-    'Private Sub btnRegistrar_Click(sender As Object, e As EventArgs) Handles btnRegistrar.Click
-    '    Dim CadenaErrores As String = ""
-    '    Dim Resultado As Integer = 0
-
-
-    '    Resultado = ValidaString(tbNombre.Text, 100, False)
-    '    If Resultado > 0 Then
-    '        Select Case Resultado
-    '            Case 1
-    '                CadenaErrores &= "El Campo Nombre no permite más de 100 carácteres." & vbNewLine
-    '            Case 2
-    '                CadenaErrores &= "El Campo Nombre no puede estar vacío" & vbNewLine
-    '        End Select
-
-
-    '    End If
-
-    '    CadenaErrores = ""
-    '    Resultado = ValidaString(tbApellidos.Text, 255, True)
-    '    If Resultado > 0 Then
-    '        Select Case Resultado
-    '            Case 1
-    '                CadenaErrores &= "El Campo Nombre no permite más de 100 carácteres." & vbNewLine
-    '            Case 2
-    '                CadenaErrores &= "El Campo Nombre no puede estar vacío" & vbNewLine
-    '        End Select
-    '        tbApellidos.BackColor = Color.LightCoral
-    '        TTip.SetToolTip(tbApellidos, CadenaErrores)
-    '        ErrorProvider1.SetError(tbApellidos, CadenaErrores)
-    '    End If
-
-    '    If CadenaErrores.Length > 0 Then
-    '        MsgBox(CadenaErrores)
-    '    Else
-
-    '        MsgBox("Jugador insertado correctamente")
-    '    End If
-    'End Sub
-
-
-
-
 
     Private Sub btnRegistrar_Click(sender As Object, e As EventArgs) Handles btnRegistrar.Click
+        'Para que el formulario sea más usable hay que indicar siempre todos los errores cometidos
+
         If _ControlNombre And _ConrtrolApellidos Then
             MsgBox("Jugador insertado correctamente")
         Else
@@ -109,33 +71,40 @@
                 Case 3
                     CadenaErrores &= "El Campo Nombre no puede ser Rojo." & vbNewLine
             End Select
+            IndicarError(tbNombre, CadenaErrores)
         Else
             _ControlNombre = True
+            IndicarOk(tbNombre)
         End If
-        IndicarError(tbNombre, CadenaErrores)
+
     End Sub
 
     Private Sub tbApellidos_Leave(sender As Object, e As EventArgs) Handles tbApellidos.Leave
         Dim CadenaErrores As String = ""
         Dim Resultado As Integer = 0
 
-        _ControlNombre = False
+        _ConrtrolApellidos = False
         Resultado = ValidaString(tbNombre.Text, 255, True)
         If Resultado > 0 Then
             Select Case Resultado
                 Case 1
-                    CadenaErrores &= "El Campo Nombre no permite más de 100 carácteres." & vbNewLine
+                    CadenaErrores &= "El Campo Apellidos no permite más de 255 carácteres." & vbNewLine
                 Case 2
-                    CadenaErrores &= "El Campo Nombre no puede estar vacío" & vbNewLine
+                    CadenaErrores &= "El Campo Apellidos no puede estar vacío" & vbNewLine
                 Case 3
-                    CadenaErrores &= "El Campo Nombre no puede ser Rojo." & vbNewLine
+                    CadenaErrores &= "El Campo Apellidos no puede ser Rojo." & vbNewLine
             End Select
+            IndicarError(tbApellidos, CadenaErrores)
         Else
-            _ControlNombre = True
+            _ConrtrolApellidos = True
+            IndicarOk(tbApellidos)
         End If
-        IndicarError(tbApellidos, CadenaErrores)
+
     End Sub
+
     Private Sub frmRegistro_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Inicializamos el estado de los controles con el formulario vacío.
         _ControlNombre = False
         _ConrtrolApellidos = True
     End Sub
